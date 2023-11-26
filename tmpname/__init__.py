f5a7f5b9a4587512414b3cbd969e84d448d0040e
ChengYong Tham <25361260+cytham@users.noreply.github.com>
2023-11-27 00:27:07 +0800

    Update __init__.py

--- tmpname/__init__.py
+++ tmpname/__init__.py
@@ -1,5 +1,5 @@
 from .version import __version__
-from .utils import get_args
-from .create_consensus import create_cons
+from .utils import get_args, check_args, check_exe
 from .create_fasta import create_fa
+from .create_consensus import create_cons
 from .repeat_annotate import rep_annote
