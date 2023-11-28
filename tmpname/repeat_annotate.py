550f7e507314788c1fcee56872810ef558a33997
ChengYong Tham <25361260+cytham@users.noreply.github.com>
2023-11-29 01:02:13 +0800

    Update repeat_annotate.py

--- tmpname/repeat_annotate.py
+++ tmpname/repeat_annotate.py
@@ -8,8 +8,7 @@ import subprocess
 def rep_annote(wk_dir, samplename, threads_per_job, species):
     input_sequence = os.path.join(wk_dir, f"{samplename}.ins.con.fasta")
     rm_dir = os.path.join(wk_dir, "rm_output")
-    if not os.path.exists(rm_dir):
-          os.makedirs(rm_dir)
+    os.makedirs(rm_dir, exist_ok=True)
     # species = species
     subprocess.run([f"RepeatMasker -species {species} -s -pa {threads_per_job} {input_sequence} -dir {rm_dir}"],
                    capture_output=True, text=True, shell = True)
