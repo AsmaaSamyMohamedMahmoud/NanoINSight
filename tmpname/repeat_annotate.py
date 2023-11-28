07e38fd0107ef0683826082862c7e911fbe90ac9
ChengYong Tham <25361260+cytham@users.noreply.github.com>
2023-11-29 01:10:09 +0800

    Update repeat_annotate.py

--- tmpname/repeat_annotate.py
+++ tmpname/repeat_annotate.py
@@ -5,11 +5,11 @@ import subprocess
 #(3) Annotation using RepeatMasker 
 ################################################################################################################################################################    
 
-def rep_annote(wk_dir, samplename, threads_per_job, species):
-    input_sequence = os.path.join(wk_dir, f"{samplename}.ins.con.fasta")
+def rep_annote(wk_dir, con_fasta, threads_per_job, species):
+    # input_sequence = os.path.join(wk_dir, f"{samplename}.ins.con.fasta")
     rm_dir = os.path.join(wk_dir, "rm_output")
     os.makedirs(rm_dir, exist_ok=True)
     # species = species
-    subprocess.run([f"RepeatMasker -species {species} -s -pa {threads_per_job} {input_sequence} -dir {rm_dir}"],
-                   capture_output=True, text=True, shell = True)
+    subprocess.run([f"RepeatMasker -species {species} -s -pa {threads_per_job} {con_fasta} -dir {rm_dir}"],
+                   capture_output=True, text=True, shell=True, check=True)
     # return {input_dir, MA_dir, con_dir, f"{samplename}.ins.con.fasta", RM_dir}
