44f9989e6cf7e84c3ed7e84c8183c52c29cfe47f
ChengYong Tham <25361260+cytham@users.noreply.github.com>
2023-11-29 11:17:04 +0800

    Update tmpname.py

--- tmpname/tmpname.py
+++ tmpname/tmpname.py
@@ -12,13 +12,19 @@ def annotate_ins(vcf,
                  sv_sup='sv_support_reads.tsv'
                  ):
     tmpname.check_args(species)
-    mafft_exe = tmpname.check_exe(mafft_exe, 'mafft') 
+    mafft_exe = tmpname.check_exe(mafft_exe, 'mafft')
+    print('Creating insertion fasta')
     id_seq, fasta_dir = tmpname.create_fa(vcf, wk_dir, sv_sup, ins_seq)
-    samplename, threads_per_job= tmpname.create_cons(vcf, wk_dir, fasta_dir, id_seq, threads, mafft_exe, batch_size=100, num_parallel_workers=5)
-    tmpname.rep_annote(wk_dir, samplename, threads_per_job, species)
+    print('Generating insertion sequence consensus')
+    con_fasta, threads_per_job= tmpname.create_cons(vcf, wk_dir, fasta_dir, id_seq, threads, mafft_exe, batch_size=100, num_parallel_workers=5)
+    print('Annotating insertions with RepeatMasker')
+    tmpname.rep_annote(wk_dir, con_fasta, threads_per_job, species)
 
 def main():
     args = tmpname.get_args()
+    # Observe verbosity
+    if args.quiet:
+        sys.stdout = open(os.devnull, 'w')
     os.makedirs(args.dir, exist_ok=True)
     annotate_ins(args.vcf, args.dir, args.species, args.threads, args.mafftpath, args.insfa, args.suptsv)
 
