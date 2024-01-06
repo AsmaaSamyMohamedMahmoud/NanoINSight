400ab17f93ead302a33e97ac6ed14b0fc7e64247
ChengYong Tham <25361260+cytham@users.noreply.github.com>
2024-01-06 23:04:47 +0800

    Update package name

--- nanoinsight/nanoinsight.py
+++ nanoinsight/nanoinsight.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import os
-import INSight
+import nanoinsight
 
 def annotate_ins(vcf, 
                  wk_dir, 
@@ -11,18 +11,18 @@ def annotate_ins(vcf,
                  ins_seq='ins_seq.fa', 
                  sv_sup='sv_support_reads.tsv'
                  ):
-    INSight.check_args(species)
-    mafft_exe = INSight.check_exe(mafft_exe, 'mafft')
+    nanoinsight.check_args(species)
+    mafft_exe = nanoinsight.check_exe(mafft_exe, 'mafft')
     print('Creating insertion fasta')
-    id_seq, fasta_dir = INSight.create_fa(vcf, wk_dir, sv_sup, ins_seq)
+    id_seq, fasta_dir = nanoinsight.create_fa(vcf, wk_dir, sv_sup, ins_seq)
     print('Generating insertion sequence consensus')
-    con_fasta, threads_per_job= INSight.create_cons(vcf, wk_dir, fasta_dir, id_seq, threads, mafft_exe, batch_size=100, num_parallel_workers=5)
+    con_fasta, threads_per_job = nanoinsight.create_cons(vcf, wk_dir, fasta_dir, id_seq, threads, mafft_exe, batch_size=100, num_parallel_workers=5)
     print('Annotating insertions with RepeatMasker')
-    INSight.rep_annote(wk_dir, con_fasta, threads_per_job, species)
+    nanoinsight.rep_annote(wk_dir, con_fasta, threads_per_job, species)
     print('Finished')
 
 def main():
-    args = INSight.get_args()
+    args = nanoinsight.get_args()
     # Observe verbosity
     if args.quiet:
         sys.stdout = open(os.devnull, 'w')
