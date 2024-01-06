3d81f3c3bda356218e7100d01aaa9f7671d4fa85
ChengYong Tham <25361260+cytham@users.noreply.github.com>
2024-01-06 23:24:00 +0800

    Added rm path

--- nanoinsight/repeat_annotate.py
+++ nanoinsight/repeat_annotate.py
@@ -5,12 +5,12 @@ import subprocess
 #(3) Annotation using RepeatMasker 
 ################################################################################################################################################################    
 
-def rep_annote(wk_dir, con_fasta, threads_per_job, species):
+def rep_annote(wk_dir, con_fasta, threads_per_job, species, repmask_exe):
     # input_sequence = os.path.join(wk_dir, f"{samplename}.ins.con.fasta")
     rm_dir = os.path.join(wk_dir, "rm_output")
     os.makedirs(rm_dir, exist_ok=True)
     # species = species
-    p = subprocess.run([f"RepeatMasker -species {species} -s -pa {threads_per_job} {con_fasta} -dir {rm_dir} -e rmblast"],
+    p = subprocess.run([f"{repmask_exe} -species {species} -s -pa {threads_per_job} {con_fasta} -dir {rm_dir} -e rmblast"],
                    capture_output=True, text=True, shell=True)
     if p.returncode != 0:
         err = p.stderr.strip()
